e3513d5b4a90831f9c2e305482723c0fa05294ff
BiPi2016 <bp.singh@hotmail.com>
2020-05-06 12:24:57 +0200

    global css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../../components/Layout';
 
 const FirstPost = () => (
-    <>
+    <Layout>
+        <Head>
+            <title>First post page</title>
+        </Head>
         <h1>This is the first post</h1>
         <p>
             <Link href="/">
                 <a>Back to home!</a>
             </Link>
         </p>
-    </>
+    </Layout>
 )
 
 export default FirstPost;
